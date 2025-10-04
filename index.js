0237e72da57b1dfb1d8134c60de00d56b511c2a4
Ivanrdz2409 <ivanrdz2409@gmail.com>
2025-10-03 20:39:01 -0600

    first commit

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const botonAuto = document.getElementById("btnAuto");
   const botonstonks = document.getElementById("btnstonks");
+  const btnsyg = document.getElementById("btnsyg");
 
+
+  if (btnsyg) {
+    btnsyg.addEventListener("click", () => {
+      window.location.href = "sygtb.html";
+    }); sygtb.html
+  }
 
   if (botonAuto) {
     botonAuto.addEventListener("click", () => {
