10e43987de4b0e16c40ea24c5bbbd5f0753c26e0
Ivanrdz2409 <ivanrdz2409@gmail.com>
2025-10-03 20:46:53 -0600

    first commit

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-
 document.addEventListener("DOMContentLoaded", () => {
+const botonAuto = document.getElementById("btnAuto");
+const botonstonks = document.getElementById("btnstonks");
+const btnsyg = document.getElementById("btnsyg");
 
-  const botonAuto = document.getElementById("btnAuto");
-  const botonstonks = document.getElementById("btnstonks");
-  const btnsyg = document.getElementById("btnsyg");
+if (btnsyg) {
+btnsyg.addEventListener("click", () => {
+window.location.href = "sygtb.html";
+});
+}
 
+if (botonstonks) {
+botonstonks.addEventListener("click", () => {
+window.location.href = "atcdstonks.html";
+});
+}
 
-  if (btnsyg) {
-    btnsyg.addEventListener("click", () => {
-      window.location.href = "sygtb.html";
-    }); sygtb.html
-  }
-
-  if (botonAuto) {
-    botonAuto.addEventListener("click", () => {
-      window.location.href = "atcdo.html";
-    });
-  }
-
-  if (botonstonks) {
-    botonstonks.addEventListener("click", () => {
-      window.location.href = "atcdstonks.html";
-    });
-  }
-})
+if (botonAuto) {
+botonAuto.addEventListener("click", () => {
+window.location.href = "atcdo.html";
+});
+}
+});
